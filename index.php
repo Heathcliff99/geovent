0abf6ac61beb9867691a3487a7502d2747ae9677
Heathcliff99 <samueldecristofaro@gmail.com>
2015-11-19 20:12:10 +0100

    -

--- index.php
+++ index.php
@@ -3,13 +3,42 @@
     <head>
         <title>Geovent</title>
         <style type="text/css">
-            body
-            {
+            body {
                 margin: 0px;
                 overflow: hidden;
             }
+            h1 {
+                position: absolute;
+                left: 50px;
+            }
+            #logIn {
+                position: absolute;
+                right: 0px;
+            }
+            form {
+                display: flex;  
+            }
+            h3 {
+                text-align: center;
+            }
         </style>
     </head>
+    <header>
+        <h1>Geovent</h1>
+        <div id="logIn">
+            <!--<h3>Log In:</h3>-->
+            <form>
+                <div>
+                    Username<br>
+                    <input type="text" name="usrn">
+                </div>
+                <div>
+                    Password<br>
+                    <input type="text" name="pwd">
+                </div>
+            </form>
+        </div>
+    </header>
     <body>
         
     </body>
