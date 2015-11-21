4704edb9f53e7f37758fc7b732f727470348eb12
Heathcliff99 <samueldecristofaro@gmail.com>
2015-11-21 22:39:34 +0100

    -

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+    <meta http-equiv="refresh" content="1" > 
     <head>
         <title>Geovent</title>
         <style type="text/css">
@@ -10,55 +11,96 @@
             h1 {
                 position: absolute;
                 left: 70px;
+                margin-top: 28px;
             }
             #logIn {
-                position: absolute;
-                right: 10px;
+                display: flex; 
             }
-            form {
-                display: flex;  
+            form[action="logIn.php"]{
+                position: absolute;
+                right: 35px;
+            }
+            form[action="logIn.php"] input[type="submit"]{
+                width: 60px;
+                display: block;
+                margin-left: auto ;
+                margin-right: auto ;
             }
             h3 {
                 text-align: center;
             }
-            #placeholder {
+            .placeholder {
                 width: 10px;
             }
             header {
+                color: #66CCFF;
                 display: block;
-                background-color: brown;
+                background-color: #993300;
                 width: 100%;
-                height: 90px;
+                height: 110px;
             }
-            #signIn {
+            #signUp {
                 text-align: center;
+                /*background-color: green;*/
+                width: 600px;
+                margin-left: auto ;
+                margin-right: auto ;
+            }
+            #signUp form {
+                /*background-color: red;*/
+                width: 100%;
+            }
+            #signUp form div {
+                display: inline;
+                /*background-color: blue;*/
+                width: 400px;
+                margin-left: auto ;
+                margin-right: auto ;
+            }
+            #email {
+                width: 315px;
+            }
+            input {
+                border-radius: 5px;
+                border-color: gray;
+                
             }
         </style>
     </head>
     <body>
         <header>
         <h1>Geovent</h1>
-        <div id="logIn">
-            <form>
-                <div>
-                    <br>
-                    Username<br>
-                    <input type="text" name="usrn">
-                </div>
-                <div id="placeholder"></div>
-                <div>
-                    <br>
-                    Password<br>
-                    <input type="text" name="pwd">
-                </div>
+            <form action="logIn.php">
+                <div id="logIn">
+                    <div>
+                        <br>
+                        Username<br>
+                        <input type="text" name="usrn">
+                    </div>
+                    <div class="placeholder"></div>
+                    <div>
+                        <br>
+                        Password<br>
+                        <input type="text" name="pwd">
+                    </div>
+                </div><br>
+                <input type="submit" value="Log in">
             </form>
-        </div>
         </header>
-        <div id="signIn">
-            <h2>Sign In:</h2><br><br>
-            <form>
-                <input type="text" name="fName" placeholder="First name">
-                <input type="text" name="lName" placeholder="Last name">
+        <div id="signUp">
+            <br><br><br><br><br><br>
+            <h2>Sign up:</h2><br><br>
+            <form action="signUp.php">
+                <div>
+                    <input type="text" name="fName" placeholder="First name">
+                    &nbsp;
+                    <input type="text" name="lName" placeholder="Last name"><br><br>
+                    <input id="email" type="email" name="email" placeholder="Email"><br><br>
+                    <input type="password" name="pw" placeholder="Password">
+                    &nbsp;
+                    <input type="password" name="pw2" placeholder="Re-enter password"><br><br>
+                    <input type="submit" value="Sign up">
+                </div>
             </form>
         </div>
     </body>
